a7e0e00b20bf2263e44bd40a2f96548a32e164f0
hikaori <kaori55194@gmail.com>
2018-10-19 11:55:53 -0700

    5-1. PortFolioItem Componentを作成してImportで読み込む

---
+++ my-app/src/components/PortfolioItem/PortfolioItem.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class PortfoliItem extends Component{
+  render(){
+    return(
+      <div className="PorfolioItem">
+      Hi
+      </div>
+    )
+  }
+}
